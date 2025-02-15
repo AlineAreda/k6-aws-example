bd36dcaa0309910ed1759c6a04e8351ee559f673
Aline Areda <aredaaline@gmail.com>
2025-02-15 13:57:19 -0300

    testes reporte

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { check } from 'k6';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js"; //reporte
 
 export const options = {
     vus: 100,
